2dd5169069d60bb207a1ee3440c614d0883866fb
alex <alexxpyykonen@gmail.com>
2024-04-29 13:47:27 +0300

    = main.cpp Create simplified functions for the actions
    = modify the platformio.ini
    + little fs create filesys and partition
    + for website data

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <DNSServer.h>
+#include <LittleFS.h>
 #include <WebServer.h>
 #include <WiFi.h>
 
@@ -10,10 +11,58 @@ WebServer	server(80);
 void		setupWebServer(void);
 void		scanAndSetupAP(void);
 
+void	listFilesInDir(File dir, int numTabs = 1)
+{
+	File	entry;
+
+	while (true)
+	{
+		entry = dir.openNextFile();
+		if (!entry)
+			break ;
+		for (int i = 0; i < numTabs; i++)
+			Serial.print('\t');
+		Serial.print(entry.name());
+		if (entry.isDirectory())
+		{
+			Serial.println("/");
+			listFilesInDir(entry, numTabs + 1);
+		}
+		else
+		{
+			Serial.print("\t\t");
+			Serial.println(entry.size(), DEC);
+		}
+		entry.close();
+	}
+}
+
+void	initLittleFS(void)
+{
+	File	root;
+
+	if (!LittleFS.begin())
+	{
+		Serial.println("Failed to mount file system. Attempting to format...");
+		if (!LittleFS.format())
+			Serial.println("Failed to format file system. Please check the hardware.");
+		if (!LittleFS.begin())
+			Serial.println("Failed to mount file system even after formatting. Exiting...");
+	}
+	Serial.println("Listing directory contents:");
+	root = LittleFS.open("/");
+	listFilesInDir(root);
+	root.close();
+}
+
 void	setup(void)
 {
 	Serial.begin(115200);
+	while (!Serial)
+		continue ;
 	Serial.println("Serial initialized.");
+	initLittleFS();
+	Serial.println("File system mounted successfully.");
 	scanAndSetupAP();
 	dnsServer.start(DNS_PORT, "*", apIP);
 	setupWebServer();
@@ -62,15 +111,42 @@ void	scanAndSetupAP(void)
 	}
 }
 
+void	serveFile(String path)
+{
+	File	file;
+	String	fileContent;
+
+	Serial.print("Requested path: ");
+	Serial.println(path);
+	file = LittleFS.open(path, "r");
+	if (!file)
+	{
+		Serial.println("File not found on LittleFS. Sending 404.");
+		server.send(404, "text/plain", "File not found");
+		return ;
+	}
+	fileContent = "";
+	while (file.available())
+	{
+		fileContent += (char)file.read();
+	}
+	file.close();
+	Serial.println("File found. Sending content...");
+	server.send(200, "text/html", fileContent);
+}
+
 void	setupWebServer(void)
 {
-	server.on("/", HTTP_GET, []() { server.send(200, "text/html",
-			"<h1>Welcome to ESP32-C3 AP!</h1><p>Please log in!</p>"); });
+	server.on("/", HTTP_GET, []() {
+		// if (WiFi.SSID().indexOf("Hive") != -1)
+		serveFile("/42login.html");
+		// else
+		//	server.send(200, "text/html",
+		//		"<h1>Welcome to ESP32-C3 AP!</h1><p>Please log in!</p>");
+	});
 	server.onNotFound([]() {
 		server.sendHeader("Location", "/", true);
-		// Redirect to the root directory
 		server.send(302, "text/plain", "");
-		// Use HTTP code 302 for redirection
 	});
 	server.begin();
 	Serial.println("Web server started, captive portal active.");
